eb9f474d57882f1204936597eabecfdf084cd05f
Jaymee Casabuena <jaymeecasabuena@gmail.com>
2024-08-27 05:06:10 +1000

    Implemented logo and sidebar; created reusable star-rating togglable

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SideBarComponent } from "./shared/components/side-bar/side-bar.component";
+import { HomeComponent } from './modules/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HomeComponent, SideBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
