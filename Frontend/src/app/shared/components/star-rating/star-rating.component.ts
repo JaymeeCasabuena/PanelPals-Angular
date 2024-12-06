1ec8d8866fd4aedc591bfefadc03f84fbc74846a
Jaymee Casabuena <jaymeecasabuena@gmail.com>
2024-12-06 23:37:10 +1000

    Created star rating shared component

--- Frontend/src/app/shared/components/star-rating/star-rating.component.ts
+++ Frontend/src/app/shared/components/star-rating/star-rating.component.ts
@@ -1,12 +1,55 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-star-rating',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './star-rating.component.html',
-  styleUrl: './star-rating.component.css'
+  styleUrl: './star-rating.component.css',
 })
 export class StarRatingComponent {
-  bookRatings = '1089';
+  @Input() rating = 0;
+  @Input() maxStars = 5;
+  @Input() readOnly = false;
+  @Output() ratingChange = new EventEmitter<number>();
+
+  stars: boolean[] = [];
+
+  ngOnInit() {
+    this.updateStars();
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['rating']) {
+      this.updateStars();
+    }
+  }
+
+  private updateStars() {
+    this.stars = Array.from(
+      { length: this.maxStars },
+      (_, i) => i < this.rating
+    );
+  }
+
+  onStarClick(index: number) {
+    if (!this.readOnly) {
+      this.rating = index + 1;
+      this.updateStars();
+      this.ratingChange.emit(this.rating);
+    }
+  }
+
+  clearRating() {
+    this.rating = 0;
+    this.updateStars();
+    this.ratingChange.emit(this.rating);
+  }
 }
