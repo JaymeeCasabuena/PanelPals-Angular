13b380020c3b7ca9d71d458ff530441aedb347ed
Jaymee Casabuena <jaymeecasabuena@gmail.com>
2024-12-20 23:46:54 +1000

    Applied changes to the frontend - renamed components and variables

--- Frontend/src/app/modules/home/home.component.ts
+++ Frontend/src/app/modules/home/home.component.ts
@@ -1,18 +1,16 @@
-import {
-  Component,
-  AfterViewInit,
-  OnInit,
-  ElementRef,
-  ViewChild,
-} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import Flickity from 'flickity';
 import { SideBarComponent } from '../../shared/components/side-bar/side-bar.component';
 import { SearchBarComponent } from '../../shared/components/search-bar/search-bar.component';
-import { BookService } from '../../shared/services/book-services/book.service';
-import { Book } from '../../shared/interfaces/book';
+import { ComicService } from '../../shared/services/comic-services/comic.service';
+import { UserService } from '../auth/services/user.service';
+import { Comic } from '../../shared/interfaces/comic';
 import { AddNewFormComponent } from '../books/add-new-book/add-new-form/add-new-form.component';
+import { MatTabsModule } from '@angular/material/tabs';
+import { Carousel } from 'primeng/carousel';
+import { TabsModule } from 'primeng/tabs';
+import { Avatar } from 'primeng/avatar';
 
 interface PopularBooks {
   title: string;
@@ -29,47 +27,78 @@ interface PopularBooks {
     SideBarComponent,
     SearchBarComponent,
     AddNewFormComponent,
+    MatTabsModule,
+    Carousel,
+    TabsModule,
+    Avatar,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent implements AfterViewInit, OnInit {
-  @ViewChild('flickityContainer', { static: false })
-  flickityContainer!: ElementRef;
-  books: Book[] = [];
-  book: any;
+export class HomeComponent implements OnInit {
+  comics: Comic[] = [];
+  comic: any;
+  currentUser: any;
 
-  constructor(private bookService: BookService, private router: Router) {}
-
-  ngAfterViewInit() {
-    setTimeout(() => {
-      const flickity = new Flickity(this.flickityContainer.nativeElement, {
-        wrapAround: true,
-        groupCells: 3, // Groups three cells per slide
-        cellAlign: 'left', // Align items horizontally
-        contain: true, // Prevent overflowing cells
-      });
-
-      this.bookService.getAllBooks().subscribe(() => {
-        flickity.reloadCells();
-      });
-    });
-  }
+  constructor(
+    private comicService: ComicService,
+    private userService: UserService,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
-    this.fetchAllBooks();
+    this.fetchAllComics();
+    this.getCurrentUser();
   }
 
-  fetchAllBooks(): void {
-    this.bookService.getAllBooks().subscribe({
-      next: (response) => (this.books = response.slice(0, 5)),
-      error: (error) => console.error('Error fetching books', error),
-      complete: () => console.log('Fetching books complete'),
+  getCurrentUser(): void {
+    this.userService.getCurrentUser().subscribe({
+      next: (response) => {
+        this.currentUser = response.data;
+      },
+      error: (error) => {
+        console.error('Error fetching current user', error);
+      },
     });
   }
 
-
-  navigateToDetails(bookId: number): void {
-    this.router.navigate(['/book-details', bookId]);
+  fetchAllComics(): void {
+    this.comicService.getAllComics().subscribe({
+      next: (response) => (this.comics = response),
+      error: (error) => console.error('Error fetching comics', error),
+      complete: () => console.log('Fetching comics complete'),
+    });
   }
+
+  navigateToDetails(comicId: number): void {
+    this.router.navigate(['/comic-details', comicId]);
+  }
+
+  newDiscussions = [
+    {
+      id: 1,
+      title: 'Recommend me a good Rofan?',
+      responses: 5,
+    },
+    {
+      id: 2,
+      title: 'When is the next update of Operation: True Love',
+      responses: 10,
+    },
+  ];
+
+  trendingDiscussions = [
+    {
+      id: 3,
+      title: 'Placeholder',
+      responses: 20,
+    },
+    {
+      id: 4,
+      title: 'Placeholder',
+      responses: 15,
+    },
+  ];
+
+  openDiscussion(id: number): void {}
 }
