bda308409813a1e36a821c8a636a3a91d5190443
Jaymee Casabuena <jaymeecasabuena@gmail.com>
2024-12-30 23:11:02 +1000

    Completed edit profile feature

--- Frontend/src/app/modules/profile/edit-profile-form/edit-profile-form.component.ts
+++ Frontend/src/app/modules/profile/edit-profile-form/edit-profile-form.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import {
   FormBuilder,
   FormGroup,
@@ -6,6 +6,7 @@ import {
   Validators,
 } from '@angular/forms';
 import { NgbActiveModal, NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ProfileService } from '../services/profile.service';
 
 @Component({
   selector: 'app-edit-profile-form',
@@ -15,16 +16,42 @@ import { NgbActiveModal, NgbModule } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./edit-profile-form.component.css'],
 })
 export class EditProfileFormComponent {
+  @Input() currentUser: any;
   editProfileForm: FormGroup;
 
-  constructor(private fb: FormBuilder, public activeModal: NgbActiveModal) {
+  constructor(
+    private fb: FormBuilder,
+    public activeModal: NgbActiveModal,
+    private profileService: ProfileService
+  ) {
     this.editProfileForm = this.fb.group({
-      userName: ['', Validators.required],
+      username: ['', Validators.required],
       avatar: ['', Validators.required],
     });
   }
 
-  onSubmit(): void {}
+  ngOnInit(): void {
+    if (this.currentUser) {
+      this.editProfileForm.patchValue({
+        username: this.currentUser.Username,
+        avatar: this.currentUser.Avatar,
+      });
+    }
+  }
+  onSubmit(): void {
+    if (this.editProfileForm.valid) {
+      const profileData = this.editProfileForm.value;
+      profileData.userId = this.currentUser.Id;
+
+      this.profileService.editProfile(profileData).subscribe({
+        next: (response) =>
+          console.log('Profile updated successfully', response),
+        error: (error) => console.error('Error updating profile', error),
+      });
+    } else {
+      console.log('Form is invalid');
+    }
+  }
 
   closeModal(): void {
     this.activeModal.dismiss();
--- Frontend/src/app/shared/components/avatar/avatar/avatar.component.ts
+++ Frontend/src/app/shared/components/avatar/avatar/avatar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ModalService } from '../../../services/modal-service/modal.service';
 import { EditProfileFormComponent } from '../../../../modules/profile/edit-profile-form/edit-profile-form.component';
 import { Avatar } from 'primeng/avatar';
@@ -11,10 +11,11 @@ import { Avatar } from 'primeng/avatar';
   styleUrl: './avatar.component.css',
 })
 export class AvatarComponent {
+  @Input() currentUser: any;
   constructor(private modalService: ModalService) {}
 
   openModal() {
     const modalRef = this.modalService.openModal(EditProfileFormComponent);
-    modalRef.componentInstance.data = { sampleData: 'Test Data' };
+    modalRef.componentInstance.currentUser = this.currentUser;
   }
 }
