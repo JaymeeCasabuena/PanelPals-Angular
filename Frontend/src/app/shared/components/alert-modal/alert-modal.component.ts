6af8435592483ab3f36cde07575851514fe56f5c
Jaymee Casabuena <jaymeecasabuena@gmail.com>
2024-12-30 23:49:13 +1000

    Created shared alert modal component

---
+++ Frontend/src/app/shared/components/alert-modal/alert-modal.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgbActiveModal, NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-alert-modal',
+  standalone: true,
+  imports: [CommonModule, NgbModule],
+  templateUrl: './alert-modal.component.html',
+  styleUrl: './alert-modal.component.css',
+})
+export class AlertModalComponent {
+  @Input() data: { title?: string; message?: string; icon?: string } = {};
+
+  constructor(public activeModal: NgbActiveModal) {}
+
+  closeModal(): void {
+    this.activeModal.dismiss();
+  }
+}
