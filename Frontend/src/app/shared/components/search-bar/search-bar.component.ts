fec403a8124f0fbebd9d8a273cb68102fa7a2c2c
Jaymee Casabuena <jaymeecasabuena@gmail.com>
2025-01-11 02:33:39 +1000

    Feature: Implemented live search

--- Frontend/src/app/shared/components/search-bar/search-bar.component.ts
+++ Frontend/src/app/shared/components/search-bar/search-bar.component.ts
@@ -1,12 +1,54 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  Observable,
+  Subject,
+  catchError,
+  debounceTime,
+  distinctUntilChanged,
+  of,
+  switchMap,
+} from 'rxjs';
+import { Router } from '@angular/router';
+import { ComicService } from '../../services/comic-services/comic.service';
+import { Comic } from '../../interfaces/comic';
 
 @Component({
   selector: 'app-search-bar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './search-bar.component.html',
-  styleUrl: './search-bar.component.css'
+  styleUrl: './search-bar.component.css',
 })
 export class SearchBarComponent {
+  searchTerm = new Subject<string>();
+  searchResults$: Observable<Comic[]> = this.searchTerm.pipe(
+    debounceTime(300),
+    distinctUntilChanged(),
+    switchMap((searchTerm) => {
+      if (!searchTerm.trim()) {
+        return of([]);
+      }
+      return this.comicService.searchComics(searchTerm).pipe(
+        catchError((error) => {
+          if (error.status === 404) {
+            console.error('No results found for search term.');
+            return of([]);
+          }
+          return of([]);
+        })
+      );
+    })
+  );
+
+  constructor(private comicService: ComicService, private router: Router) {}
+
+  onInputChange(e: any): void {
+    this.searchTerm.next(e.target.value);
+  }
+
+  navigateToDetails(comicId: number): void {
+    this.router.navigate(['/comic-details', comicId]);
+  }
 
 }
